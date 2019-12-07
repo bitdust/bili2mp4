657e47c51995ef5c66d186fcc15ad1fee3acff44
bitdust <github@bitdust.me>
2019-12-07 16:29:21 +0800

    Add: the world.

--- README.md
+++ README.md
@@ -1,2 +1,37 @@
-# bili2mp4
-将bilibili安卓客户端的离线缓存转换为mp4.
+# bili2mp4  
+将bilibili安卓客户端的离线缓存转换为mp4.  
+基于 ffmpeg 实现  
+
+# 下载地址
+本项目只提供windows版本的独立可执行程序，下载地址在此---->[点我!](https://github.com/bitdust/bili2mp4/releases)
+
+# 使用方法  
+1. 在bilibili安卓客户端离线你所要的视频  
+2. 连接手机和电脑，进入手机的 `Android\data\tv.danmaku.bili\download` 目录
+3. 这个目录中的每一个文件夹都对应着一个离线视频，可以根据修改日期找到您刚离线的视频文件夹
+4. 将文件夹复制到您的电脑中
+5. 将复制出来的文件夹拖放到`bili2mp4.exe`图标上
+6. 稍等片刻，当前目录中会生成一个`bili2mp4_output`文件夹，转换后的视频就会输出到这里
+7. 一同输出的还包括xml弹幕文件，推荐使用<弹弹play>播放器播放视频和弹幕.
+
+# 使用演示
+![使用演示](.\example.gif)
+
+# 打包&进阶使用
+## 可以在linux下使用吗？
+我尽力把代码写的跨平台了，但并没有测试。您可以试试在linux下安装ffmpeg后，直接使用纯python脚本方式运行。
+## 如何使用纯python脚本执行？
+也许你在使用linux或者并不想使用打包的exe，那么可以直接使用python运行本脚本:  
+```
+python bili2mp4.py <视频文件夹>
+```
+当然，需要python3.5以上运行环境，同样依赖ffmpeg，请下载ffmepg到本脚本同级目录或安装ffmpeg到系统PATH中即可。
+## 如何打包为单独可执行文件？
+本项目依赖 ffmpeg 工具，因此需要下载 ffmpeg 到项目根目录;  
+然后使用pyinstaller打包即可输出exe文件，打包命令为:
+```
+pyinstaller -F bili2mp4.spec
+```
+exe文件保存在dist目录下。
+## 为什么有人在有电脑的情况下还想用bilibili安卓客户端下载视频？是电脑不能上网吗还是手机话费太多了？  
+那就问问这个世界为什么会这么操蛋吧~  
---
+++ bili2mp4.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+# bilibili 手机离线缓存转换
+import json
+import os
+import sys
+import re
+import shutil
+import argparse
+
+
+parser = argparse.ArgumentParser(formatter_class=argparse.RawTextHelpFormatter)
+parser.add_argument("video_dir", type=str, help="视频目录文件夹")
+parser.description = r"""转换bilibili安卓客户端的离线缓存视频到mp4文件. version:0.1-x64
+
+    Q: bilibili安卓客户端的缓存文件在哪里？
+    A: 该离线文件通常在手机的 'Android\data\tv.danmaku.bili\download' 文件夹中.
+    Q: 如何使用本工具进行转换？
+    A: 前述download文件夹中每个文件都对应一个视频or视频集锦，将这个视频文件夹复制到本地并运行命令: 'bili2mp4.py <视频目录文件夹>'
+    Q: 转换后的视频保存在哪里？
+    A: 所有转换的视频和弹幕文件会保存在当前路径下的 'bili2mp4_output' 文件夹.
+    Q: 如何找到我想要的视频中对应文件夹？
+    A: 我的经验是按照你的下载日期对应即可.
+    Q: 如何在播放视频时加载弹幕文件？
+    A: 推荐使用<弹弹play>播放器，可以自动识别并加载弹幕文件.
+    Q: 出问题了？
+    A: 请向 github.com/bitdust/bili2mp4 反馈并求助."""
+args = parser.parse_args()
+
+SCRIPT_DIR = os.path.split(os.path.realpath(__file__))[0] # 程序所在位置
+MAIN_DIR = os.path.abspath(args.video_dir) # 视频文件夹
+WORKING_DIR = os.getcwd()
+if not os.path.exists('bili2mp4_output'):
+    os.mkdir("bili2mp4_output")
+OUTPUT_DIR = os.path.abspath('bili2mp4_output') # 输出文件夹
+
+# 检查ffmpeg是否存在,并设置ffmpeg工具路径
+FFMPEG_CMD = ""
+if sys.platform=='win32':
+    ffmpeg_name = "ffmpeg.exe"
+else:
+    ffmpeg_name = "ffmpeg"
+if hasattr(sys,'_MEIPASS'): # 判断是否为 pyinstaller 打包环境
+    FFMPEG_CMD = os.path.join(sys._MEIPASS, ffmpeg_name)
+    if not os.path.exists(FFMPEG_CMD):
+        print(r"pyinstall打包出问题啦，ffmpeg找不到!")
+        exit(-1)
+else:
+    if os.path.exists(os.path.join(SCRIPT_DIR,ffmpeg_name)):
+        FFMPEG_CMD = os.path.join(SCRIPT_DIR,ffmpeg_name) # 优先使用自带的 ffmpeg
+    elif shutil.which(ffmpeg_name) is not None:
+        FFMPEG_CMD = ffmpeg_name
+    else:
+        print(r"找不到ffmpeg命令，请安装ffmpeg或下载ffmepg并放在同一目录下.")
+        exit(-1)
+
+def ffmpeg_concat_m4s(segments:list, video_name:str):
+    # 进入输出目录，并将指定的m4s文件使用ffmpeg进行连接，输出在同一目录中并修改文件名
+    os.chdir(OUTPUT_DIR)
+    input_str = ""
+    for segment in segments:
+        input_str += f" -i {segment}"
+    cmd_str = f"""{FFMPEG_CMD} -loglevel quiet {input_str} -c copy -y "{video_name}.mp4" """
+    status = os.system(cmd_str)
+    os.chdir(WORKING_DIR)
+    return status
+
+
+def ffmpeg_concat_blv(segments:list, video_name:str):
+    # 进入输出目录，并将指定的blv文件使用ffmpeg进行连接，输出在同一目录中并修改文件名
+    os.chdir(OUTPUT_DIR)
+    input_file = open('input.txt', 'w')
+    for segment in segments:
+        input_file.write(f"""file '{segment}' \n""")
+    input_file.close()
+    cmd_str = f"""{FFMPEG_CMD} -loglevel quiet -f concat -i input.txt -c copy -y "{video_name}.mp4" """
+    status = os.system(cmd_str)
+    os.remove('input.txt')
+    os.chdir(WORKING_DIR)
+    return status
+
+
+def build_title(entry_dict:dict):
+    # 提取标题是个很麻烦的事
+    title = entry_dict["title"].strip() # 主标题
+    if 'ep' in entry_dict.keys():
+        if entry_dict['ep']["index_title"] == '':
+            title += "-"
+            title += entry_dict['ep']["index"].strip()
+        else:
+            title += "-"
+            title += "-".join([entry_dict['ep']["index"],entry_dict['ep']["index_title"]]).strip()
+    if 'page_data' in entry_dict.keys():
+        title += "-"
+        title += entry_dict['page_data']["part"].strip()
+    title = re.sub(r"[\/\\\:\*\?\"\<\>\|]", " ", title) # 过滤文件名中的非法字符
+    return title
+
+
+def convert_episode(episode_path : str):
+    # 解析每一集视频，转换文件，提取弹幕xml并输出到output目录中
+    entry_file = os.path.join(episode_path,'entry.json') # 元信息json文件
+    with open(entry_file, 'r', encoding='utf-8') as f:
+        entry_dict = json.load(f, encoding='utf-8')
+    episode_name = build_title(entry_dict)    
+    # 输出弹幕文件
+    danmuku_path = os.path.join(episode_path,'danmaku.xml')
+    if os.path.exists(danmuku_path):
+        shutil.copy(danmuku_path, os.path.join(OUTPUT_DIR,episode_name+'.xml'))
+    # 处理视频文件
+    tag_type = entry_dict["type_tag"] # 视频的存储类型，也是视频文件的存放目录名称
+    seg_path = os.path.join(episode_path, tag_type)
+    video_segments = [i for i in os.listdir(seg_path) if 'blv' in i.split('.')[-1] or 'm4s' in i.split('.')[-1]]
+    for segment in video_segments:
+        shutil.copy(os.path.join(episode_path,tag_type,segment), OUTPUT_DIR)
+    if 'flv' in tag_type:
+        status = ffmpeg_concat_blv(video_segments, episode_name)
+    elif tag_type.isdigit():
+        status = ffmpeg_concat_m4s(video_segments, episode_name)
+    else:
+        print(f"未知格式: {tag_type} 转换失败.")
+    for segment in video_segments:
+        os.remove(os.path.join(OUTPUT_DIR,segment))
+    if status==0:
+        print(f"{episode_name} 转换完成.")
+    else:
+        print(f"{episode_name} 转换失败.")
+
+
+for item in os.listdir(MAIN_DIR):
+    # 遍历主文件夹
+    sub_dir = os.path.join(MAIN_DIR,item)
+    if os.path.isdir(sub_dir):
+        if 'entry.json' in os.listdir(sub_dir):
+            convert_episode(sub_dir) # 处理每集视频
+        else:
+            continue
